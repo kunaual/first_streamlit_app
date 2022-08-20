9783c84e5f59518d42e5711355ebd5910b333729
kunaual <73131413+kunaual@users.noreply.github.com>
2022-08-20 18:25:32 -0500

    Update streamlit_app.py
    updated header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('Diner! new menu! blahblahbalh')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
 streamlit.text('Kale, Spinach & rocket smoothie.  Blech')
 streamlit.text('Hard Boiled Free range egg')
