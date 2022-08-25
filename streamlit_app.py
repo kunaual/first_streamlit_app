08e82b15e5b2aaad2e92294f3cbe4a56c8abb52a
kunaual <73131413+kunaual@users.noreply.github.com>
2022-08-24 19:02:34 -0500

    Update streamlit_app.py
    added sf funct

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,16 +51,27 @@ except URLError as e:
 # dataframe it!
 
 
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_curwh = my_cnx.cursor()
 my_curwh.execute("use warehouse compute_wh")
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+# my_cur = my_cnx.cursor()
+
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#sf functs
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+  
+#button!
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+  streamlit.stop()
 
 add_my_fruit = streamlit.text_input('What fruit would you liketo add?','jackfruit')
 streamlit.text('Thanks for adding ' + add_my_fruit)
